7c8e43859cbe46e2b311212824df2aaed0ba2e05
CREESTL <kroymw3@yandex.ru>
2023-02-25 22:23:40 +0300

    Add upgradeable ERC20 token.

---
+++ contracts/CRSTLUpgradeable.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract CRSTL is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    
+    uint8 internal _decimals;
+
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_
+    ) initializer public {
+        __ERC20_init(name, symbol);
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        _decimals = decimals_;
+    }
+
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
