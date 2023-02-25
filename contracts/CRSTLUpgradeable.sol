4d7e1e5461fde20c82424c4351ba867e0b14ef09
CREESTL <kroymw3@yandex.ru>
2023-02-25 23:01:43 +0300

    Fix contract name

--- contracts/CRSTLUpgradeable.sol
+++ contracts/CRSTLUpgradeable.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract CRSTL is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract CRSTLUpgradeable is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
     
     uint8 internal _decimals;
 
